15acf812f838ff53ab85dbf901e5163d878236fd
ramu <payiliramu@gmail.com>
2025-03-24 17:14:12 +0530

    function

--- 06.array.sh
+++ 06.array.sh
@@ -1,9 +1,11 @@
 #!/bin/bash/
 
 movies=("rrr" "salaar" "kgf")
-#size of the array is 3
+#size of above array is 3.
 #index are 0,1,2
-#lists always starts with 0
-echo first movie is: {$movie[0]}
-echo first movie is: {$movie[1]}
-echo first movie is: {$movie[@]}
+
+#lists always starts with 0.
+
+echo "first movie is: {$movie[0]}"
+echo "first movie is: {$movie[1]}"
+echo "first movie is: {$movie[@]}"
