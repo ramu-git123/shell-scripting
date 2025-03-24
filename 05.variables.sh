7896168c15de0f58f0e3bd0064a39751c62e0e60
ramu <payiliramu@gmail.com>
2025-03-24 16:47:27 +0530

    function

---
+++ 05.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+
+echo "please enter user name"
+
+read username #username is a variable
+
+echo "please enter password"
+
+read password
+
+echo "username:$username password:$password   
