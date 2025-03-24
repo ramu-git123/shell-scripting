18ffc44d18d9afa1013b888fd7f0d33f93348d29
ramu <payiliramu@gmail.com>
2025-03-24 16:30:04 +0530

    function

--- variables.sh
+++ variables.sh
@@ -5,7 +5,7 @@ person1=$1
 person2=$2
 
 #referring the variable
-echo "person1: haii person2,how are uh"
-echo "person2: hii person1 i am fine" 
-echo "person1: are u learning devops"
-echo "person2: yes i am learing devops"
+echo "$person1: haii $person2,how are uh"
+echo "$person2: hii $person1 i am fine" 
+echo "$person1: are u learning devops"
+echo "$person2: yes i am learing devops" 
