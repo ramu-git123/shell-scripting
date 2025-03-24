a9f874984376e69cb10d53f4b97cac0e709f7335
ramu <payiliramu@gmail.com>
2025-03-24 17:09:16 +0530

    function

--- 06.array.sh
+++ 06.array.sh
@@ -4,6 +4,6 @@ movies=("rrr" "salaar" "kgf")
 #size of the array is 3
 #index are 0,1,2
 #lists always starts with 0
-echo first movie:$movie0
-echo first movie:$movie1
-echo first movie:$movie@
+echo first movie:$movie[0]
+echo first movie:$movie[1]
+echo first movie:$movie[@]
