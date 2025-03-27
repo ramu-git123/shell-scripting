3a11f46c90cdf225d6ff3d8882a6f428584979bb
ramu <payiliramu@gmail.com>
2025-03-27 13:09:18 +0530

    function

--- current-script.sh
+++ current-script.sh
@@ -2,9 +2,6 @@
 
 current= "i am learning devops in current script"
 
-echo "before calling the other script, course=$current"
-echo "process instance id of the current script"
+echo "variable calling the other script, course=$current"
+echo "process instance id of the other script"
 
-./other-script.sh
-
-echo "after calling the other script, course=$current"
